b2209569b6e9a93ac046d24cff82854035ac3c52
anyaguuu <ag156@rice.edu>
2022-12-21 20:02:42 -0500

    usermodel and scema

---
+++ pomodoro/src/UserModel.js
@@ -0,0 +1,14 @@
+import { composeWithMongoose } from "graphql-compose-mongoose";
+
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema;
+
+require("./backend");
+
+var UserSchema = new Schema({
+    name: {type: String},
+    gender: {type: String},
+})
+
+export const User = mongoose.model("names", UserSchema);
+export const UserTC = composeWithMongoose(User);
--- pomodoro/src/backend.js
+++ pomodoro/src/backend.js
@@ -1,34 +1,42 @@
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
 
-async function listDatabases(client) {
-    // databasesList = await client.db().admin().listDatabases();
+// async function listDatabases(client) {
+//     // databasesList = await client.db().admin().listDatabases();
 
-    // console.log("Databases:");
-    // databasesList.databases.forEach(db => console.log( ` -${db.name}`));
+//     // console.log("Databases:");
+//     // databasesList.databases.forEach(db => console.log( ` -${db.name}`));
 
-    // const mongodb = app.currentUser.mongoClient("mongodb-atlas");
+//     // const mongodb = app.currentUser.mongoClient("mongodb-atlas");
 
-    const names = client.db("Names").collection("name1");
+//     const names = client.db("Names").collection("name1");
 
-    const ans = await names.findOne({name:"William"});
-    console.log("name: ", ans);
-}
+//     const ans = await names.findOne({name:"William"});
+//     console.log("name: ", ans);
+// }
 
-async function main() {
-    const uri = "mongodb+srv://anyagu:Sojenya135@cluster0.tiar473.mongodb.net/test";
-    const client = new MongoClient(uri);
+// async function main() {
+//     const uri = "mongodb+srv://anyagu:Sojenya135@cluster0.tiar473.mongodb.net/test";
+//     const client = new MongoClient(uri);
 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
+//     try {
+//         // Connect to the MongoDB cluster
+//         await client.connect();
 
-        // make DB calls
-        await listDatabases(client);
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
+//         // make DB calls
+//         await listDatabases(client);
+//     } catch (e) {
+//         console.error(e);
+//     } finally {
+//         await client.close();
+//     }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
+
+let mongoose = require("mongoose");
+const uri = "mongodb+srv://anyagu:Sojenya135@cluster0.tiar473.mongodb.net/test";
+mongoose.connect(url);
+
+mongoose.connection.on("connected", function() {
+    console.log("Mongoose connected to " + url);
+})
