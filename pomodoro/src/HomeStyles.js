234543a5fb8cef1b9ea923ac080373eab8c95115
anyaguuu <ag156@rice.edu>
2023-07-10 09:30:42 -0500

    PRETTYY

--- pomodoro/src/HomeStyles.js
+++ pomodoro/src/HomeStyles.js
@@ -8,13 +8,28 @@ export const Page = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #f9f9f9;
+  background: linear-gradient(45deg, #ff7170, #ffe09d, #81d6e3);
+  background-size: 600% 600%;
+  animation: gradientAnimation 10s ease infinite;
+
+  @keyframes gradientAnimation {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
 `;
 
 export const Title = styled.div`
-  font-family: Arial;
-  color: #4e4e4e;
-  font-size: 3.5em;
+  font-family: "Arial Black", sans-serif;
+  color: #ffffff;
+  font-size: 4em;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
   margin-bottom: 1em;
 `;
 
@@ -24,10 +39,10 @@ export const TimerBox = styled(Box)`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: #ffffff;
+  background-color: rgba(255, 255, 255, 0.9);
   border-radius: 8px;
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  font-size: 5em;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  font-size: 6em;
 `;
 
 export const OuterButtonBox = styled(Box)`
@@ -38,9 +53,10 @@ export const OuterButtonBox = styled(Box)`
 `;
 
 export const ButtonBox = styled.button`
-  background-color: #5db8a3;
+  background-color: #81d6e3;
   color: #ffffff;
-  font-size: 1.6em;
+  font-family: "Arial", sans-serif;
+  font-size: 1.8em;
   border: none;
   border-radius: 4px;
   height: 4.5em;
@@ -50,6 +66,6 @@ export const ButtonBox = styled.button`
   transition: background-color 0.3s ease-in-out;
 
   &:hover {
-    background-color: #438f7d;
+    background-color: #64c2d4;
   }
 `;
