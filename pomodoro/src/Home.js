8b2da9786a5e11b0b7a4cfc4ae6671b4a61807f2
anyaguuu <ag156@rice.edu>
2023-07-13 14:49:51 -0500

    delete unused favicons/logos

--- pomodoro/src/Home.js
+++ pomodoro/src/Home.js
@@ -3,7 +3,7 @@ import React, {useState, useEffect, Fragment} from 'react';
 // import { TextField} from "@material-ui/core";
 
 const START_WORK_MINUTES = '25';
-// const START_SHORT_BREAK_MINUTES = '5';
+const START_SHORT_BREAK_MINUTES = '5';
 const START_SECONDS = '00';
 const START_DURATION = 10;
 
